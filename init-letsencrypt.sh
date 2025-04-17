fc1ad34e0995aa20beb28241e24471a7ef3a1c9f
Hoang Ta <87011461+johnH872@users.noreply.github.com>
2025-04-17 18:51:46 +0700

    fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -15,9 +15,16 @@ if [ -d "$data_path" ]; then
   fi
 fi
 
+# Tạo các thư mục cần thiết
+mkdir -p "$data_path/conf"
+mkdir -p "$data_path/www"
+mkdir -p "$data_path/conf/live/$domains"
+mkdir -p "$data_path/conf/archive/$domains"
+mkdir -p "$data_path/conf/renewal"
+chmod -R 777 "$data_path"
+
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
@@ -25,8 +32,8 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
 docker compose run --rm --entrypoint "\
+  mkdir -p /etc/letsencrypt/live/$domains && \
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -60,6 +67,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker compose run --rm --entrypoint "\
+  mkdir -p /etc/letsencrypt/live/$domains && \
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
